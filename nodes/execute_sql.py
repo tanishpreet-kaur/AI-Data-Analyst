ef91c1c5edfdee1ec3162bfaac38456e5f256c90
Tanishpreet Kaur <tanish.kaur2004@gmail.com>
2026-06-22 11:47:59 +0000

    changes

--- nodes/execute_sql.py
+++ nodes/execute_sql.py
@@ -1,9 +1,11 @@
 from database.query_executer import execute_sql
 from model.llm import llm
 from states.InsightReport import InsightReport
+from utils.sql_utils import clean_sql
 
 def execute_and_generate_insights(state):
-    query_result = execute_sql(state["sql_query"])
+    sql_query = clean_sql(state["sql_query"])
+    query_result = execute_sql(sql_query)
     
     insight_agent = llm.with_structured_output(InsightReport)
     report = insight_agent.invoke(
